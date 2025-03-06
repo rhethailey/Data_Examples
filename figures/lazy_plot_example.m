a0d888074bdf6656dc0f30729615bc92df1e301d
Rhet <rhetooski@me.com>
2025-03-05 20:25:11 -0600

    added lazy plot example

---
+++ figures/lazy_plot_example.m
@@ -0,0 +1,77 @@
+clear all, clc
+
+
+% insert your color pallette of choice
+col = parula(4);
+
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+set(groot,'defaultAxesTickLabelInterpreter','latex');
+set(groot,'DefaultAxesFontSize',20);
+set(groot, 'DefaultLineLineWidth', 3);
+set(groot, 'DefaultAxesColorOrder', col); % lazy color changing
+
+dt = .01;
+t = 0:dt:2*pi;
+y1 = sin(t);
+y2 = sin(2*t);
+
+x = cos(t);
+
+
+figure(1)
+clf(1)
+
+set(figure(1), 'Units', 'Inches', 'Position', [1.5 0.25, 10, 6]); % [x, y, width, height]
+
+hold on
+grid on
+
+py1 = plot(t,y1);
+py2 = plot(t,y2);
+px = plot(t,x);
+
+l = legend([py1, py2, px],{'sin(t)','sin(2t)','cos(t)'},'Location','northoutside',...
+    'Orientation','horizontal');
+l.FontSize = 32;
+
+yl = ylabel("Output");
+xl = xlabel("Time [s]");
+
+ax = gca;
+ax.YAxis.FontSize = 32;
+ax.XAxis.FontSize = 32;
+
+
+xLIM = [0 2*pi];
+yLIM = [-1 1]*1.25
+
+set(ax, 'YTick', -1:.5:1);  % Define x-tick positions
+set(ax, 'XTick', 0:pi/2:2*pi);  % Define x-tick position
+set(ax,'XLim',xLIM)
+set(ax,'YLim',yLIM)
+
+
+xticks('auto')
+yticklabels('auto')
+
+
+%% save // LAZY
+answer = questdlg('Do you want to save your figs?', ...
+    'fig prompts', ...
+    'yes','no','no');
+
+switch answer
+    case 'yes'
+        
+        pos = get(gcf, 'Position');
+        set(gcf, 'PaperPositionMode', 'Auto', ...
+            'PaperUnits', 'Inches', ...
+            'PaperSize', [pos(3), pos(4)]);
+        
+        % Export to PDF with vector graphics and proper resolution
+        % grabs current figure
+        print(gcf, ['figs/fig6u'], '-dpdf', '-vector');
+
+    case 'no'
+end
