a8ea0f9cd22bebc64456a242446fc96997cef9c8
Rhet <rhetooski@me.com>
2025-04-18 14:16:19 -0500

    added TCP handshake bare bones

---
+++ TCP/handshake_cmake/handshake/client.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdlib>
+
+void server_wait(){
+    // server init
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // Create socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up address structure
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(12347);
+
+    // Bind socket to port
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 1) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Process 1 ready. Waiting for Process 2 to connect...\n";
+
+    // Accept connection from client (Process 2)
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Process 2 connected! Starting execution...\n";
+
+    // Cleanup
+    close(new_socket);
+    close(server_fd);
+}
+
+int main()
+{   
+    server_wait();
+
+
+    // *** run executable *** 
+    for(int i = 0; i < 10; i++){
+        std::cout << "hello client \tn:" << i << "\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    return 0;
+}
---
+++ TCP/handshake_cmake/handshake/server.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdlib>
+
+int client_wait(std::string sendip) {
+    // pre connect shit
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(12347);
+
+    // Convert IPv4 addresses from text to binary form
+    if (inet_pton(AF_INET, sendip.c_str(), &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address / Address not supported");
+        return -1;
+    }
+
+    std::cout << "Waiting for Process 1...\n";
+
+    // Attempt to connect to server
+    while (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        usleep(100000); // Wait 100ms before retrying
+    }
+
+    std::cout << "Connected to Process 1, now starting execution.\n";
+
+    // Cleanup
+    close(sock);
+
+    return 0;
+}
+
+int main()
+{
+   
+    std::cout << "Waiting..." << std::endl;
+
+    int foo;
+    std::string ip = "0.0.0.0";
+    foo = client_wait(ip);
+
+
+    // *** run executable *** 
+    for(int i = 0; i < 10; i++){
+        std::cout << "hello client \tn:" << i << "\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    return 0;
+}
